6f452c69826aa318da27786524bbea1c53f0814f
Jacob Hummer <jcbhmr@outlook.com>
2023-05-11 15:26:55 -0500

    customize package

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
-export default {
-  output: {
-    format: 'cjs',
-  },
-  plugins: [commonjs(),[nodeResolve()],
-};
